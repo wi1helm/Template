00b392b9c64b1c8db5fbb4d24e91ec378ce34612
Wi1helm <wilhelmlidblad@gmail.com>
2025-01-26 04:35:56 +0100

    A

--- src/main/java/nub/wi1helm/template/TemplateInventory.java
+++ src/main/java/nub/wi1helm/template/TemplateInventory.java
@@ -15,10 +15,12 @@ import java.util.function.BiConsumer;
 
 public abstract class TemplateInventory {
     private final Component title;
+    private final InventoryType type;
     private final Map<Integer, TemplateItem> items = new HashMap<>();
 
-    public TemplateInventory(Component name) {
+    public TemplateInventory(Component name, InventoryType type) {
         this.title = name;
+        this.type = type;
         initialize();
     }
 
@@ -30,17 +32,21 @@ public abstract class TemplateInventory {
         return title;
     }
 
+    public InventoryType getType() {
+        return type;
+    }
+
     public void setItem(int slot, TemplateItem item) {
         items.put(slot, item);
     }
 
     public void fillInventory(TemplateItem filler) {
-        for (int i = 0; i < 54; i++) { // Default inventory size to 54
+        for (int i = 0; i < type.getSize(); i++) { // Use the type's slot count
             items.putIfAbsent(i, filler);
         }
     }
 
-    public Inventory constructInventory(Player player, InventoryType type) {
+    public Inventory constructInventory(Player player) {
         Inventory inventory = new Inventory(type, title);
         construct(player, inventory::setItemStack, inventory);
         return inventory;
